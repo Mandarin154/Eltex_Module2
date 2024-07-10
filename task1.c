6338dde2b5e6b65bcabc6897460b3e58d47cc15b
Maxim <maxim432015@gmail.com>
2024-07-10 09:26:05 +0500

    Dodelat

---
+++ task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define Max_Sym 10
+
+typedef struct social
+{
+    char *email
+};
+
+
+typedef struct person
+{
+        char *firstname;
+        char *surname;
+        char *thirdname;
+        char *firstname;
+};
