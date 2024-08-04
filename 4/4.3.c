58537edffa11c5ae987c7e0572856b4d3f74cffa
Mandarin154 <95116539+Mandarin154@users.noreply.github.com>
2024-08-04 20:20:24 +0700

    Update 4.3.c

--- 4/4.3.c
+++ 4/4.3.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_CONTACTS 100
 #define MAX_SYM 100
 #define MAX_PHONE_NUM 15
 #define MAX_EMAIL_LEN 50
@@ -24,121 +23,105 @@ struct Contact {
     int messenger_count;
 };
 
-struct Contact contacts[MAX_CONTACTS];
-int contact_count = 0;
+typedef struct Node {
+    struct Contact contact;
+    struct Node *left;
+    struct Node *right;
+} Node;
 
-void addContact() {
-    struct Contact new_person;
+// Глобальная переменная для корня дерева
+Node *root = NULL;
 
-    printf("Введите имя: ");
-    scanf("%s", new_person.firstname);
+// Прототипы функций
+Node* createNode(struct Contact contact);
+Node* insert(Node* node, struct Contact contact);
+void inOrder(Node* node, int* count);
+void editContactData(struct Contact *contact);
+void editContact(Node *node, int index, int *count);
+Node* deleteNode(Node* root, int index, int *count);
+void menu();
 
-    printf("Введите фамилию: ");
-    scanf("%s", new_person.lastname);
-
-    printf("Введите отчество: ");
-    scanf("%s", new_person.thirdname);
-
-    printf("Введите место работы (необязательно): ");
-    scanf("%s", new_person.company);
-
-    printf("Введите должность (необязательно): ");
-    scanf("%s", new_person.post);
-
-    new_person.phone_count = 0;
-    printf("Введите номера телефонов (разделяйте пробелами, введите 'end' для завершения): ");
-    while (1) {
-        char phone[MAX_PHONE_NUM];
-        scanf("%s", phone);
-        if (strcmp(phone, "end") == 0) {
-            break;
-        }
-        strcpy(new_person.phone_numbers[new_person.phone_count++], phone);
-    }
-
-    new_person.email_count = 0;
-    printf("Введите адреса электронной почты (разделяйте пробелами, введите 'end' для завершения): ");
-    while (1) {
-        char email[MAX_EMAIL_LEN];
-        scanf("%s", email);
-        if (strcmp(email, "end") == 0) {
-            break;
-        }
-        strcpy(new_person.emails[new_person.email_count++], email);
-    }
-
-    new_person.social_count = 0;
-    printf("Введите ссылки на соцсети (разделяйте пробелами, введите 'end' для завершения): ");
-    while (1) {
-        char link[MAX_SOC];
-        scanf("%s", link);
-        if (strcmp(link, "end") == 0) {
-            break;
-        }
-        strcpy(new_person.social_links[new_person.social_count++], link);
-    }
-
-    new_person.messenger_count = 0;
-    printf("Введите ссылки на мессенджеры (разделяйте пробелами, введите 'end' для завершения): ");
-    while (1) {
-        char link[MAX_SOC];
-        scanf("%s", link);
-        if (strcmp(link, "end") == 0) {
-            break;
-        }
-        strcpy(new_person.messenger_links[new_person.messenger_count++], link);
-    }
-
-    contacts[contact_count++] = new_person;
-    printf("Контакт добавлен. Номер: %d\n", contact_count - 1);
+Node* createNode(struct Contact contact) {
+    Node *newNode = (Node*) malloc(sizeof(Node));
+    newNode->contact = contact;
+    newNode->left = newNode->right = NULL;
+    return newNode;
 }
 
-void editContact() {
-    if (contact_count == 0) {
-        printf("Список контактов пуст.\n");
-        return;
+Node* insert(Node* node, struct Contact contact) {
+    if (node == NULL) {
+        return createNode(contact);
     }
-
-    int index;
-    printf("Введите номер контакта для редактирования (от 0 до %d): ", contact_count - 1);
-    scanf("%d", &index);
-
-    if (index < 0 || index >= contact_count) {
-        printf("Неверный номер контакта.\n");
-        return;
+    if (strcmp(contact.lastname, node->contact.lastname) < 0) {
+        node->left = insert(node->left, contact);
+    } else {
+        node->right = insert(node->right, contact);
     }
+    return node;
+}
 
-    struct Contact *contact = &contacts[index];
+void inOrder(Node* node, int* count) {
+    if (node != NULL) {
+        inOrder(node->left, count);
+        printf("Контакт %d:\n", *count);  
+        printf("Имя: %s\n", node->contact.firstname);
+        printf("Фамилия: %s\n", node->contact.lastname);
+        printf("Отчество: %s\n", node->contact.thirdname);
+        printf("Компания: %s\n", node->contact.company);
+        printf("Должность: %s\n", node->contact.post);
+        printf("Телефоны: ");
+        for (int j = 0; j < node->contact.phone_count; j++) {
+            printf("%s ", node->contact.phone_numbers[j]);
+        }
+        printf("\nEmail: ");
+        for (int j = 0; j < node->contact.email_count; j++) {
+            printf("%s ", node->contact.emails[j]);
+        }
+        printf("\nСоцсети: ");
+        for (int j = 0; j < node->contact.social_count; j++) {
+            printf("%s ", node->contact.social_links[j]);
+        }
+        printf("\nМессенджеры: ");
+        for (int j = 0; j < node->contact.messenger_count; j++) {
+            printf("%s ", node->contact.messenger_links[j]);
+        }
+        printf("\n\n");
 
-    printf("Текущие данные контакта:\n");
+        (*count)++;
+        inOrder(node->right, count);
+    }
+}
+
+void editContactData(struct Contact *contact) {
     printf("Имя: %s\n", contact->firstname);
     printf("Фамилия: %s\n", contact->lastname);
     printf("Отчество: %s\n", contact->thirdname);
-    printf("Место работы: %s\n", contact->company);
+    printf("Компания: %s\n", contact->company);
     printf("Должность: %s\n", contact->post);
-    printf("Номера телефонов: ");
-    for (int i = 0; i < contact->phone_count; i++) {
-        printf("%s ", contact->phone_numbers[i]);
+    printf("Телефоны: ");
+    for (int j = 0; j < contact->phone_count; j++) {
+        printf("%s ", contact->phone_numbers[j]);
     }
-    printf("\nАдреса электронной почты: ");
-    for (int i = 0; i < contact->email_count; i++) {
-        printf("%s ", contact->emails[i]);
+    printf("\nEmail: ");
+    for (int j = 0; j < contact->email_count; j++) {
+        printf("%s ", contact->emails[j]);
     }
-    printf("\nСсылки на соцсети: ");
-    for (int i = 0; i < contact->social_count; i++) {
-        printf("%s ", contact->social_links[i]);
+    printf("\nСоцсети: ");
+    for (int j = 0; j < contact->social_count; j++) {
+        printf("%s ", contact->social_links[j]);
     }
-    printf("\nСсылки на мессенджеры: ");
-    for (int i = 0; i < contact->messenger_count; i++) {
-        printf("%s ", contact->messenger_links[i]);
+    printf("\nМессенджеры: ");
+    for (int j = 0; j < contact->messenger_count; j++) {
+        printf("%s ", contact->messenger_links[j]);
     }
     printf("\n");
-
+    
+    // Выбор поля для редактирования
     printf("Что вы хотите отредактировать?\n");
     printf("1. Имя\n");
     printf("2. Фамилию\n");
     printf("3. Отчество\n");
-    printf("4. Место работы\n");
+    printf("4. Компанию\n");
     printf("5. Должность\n");
     printf("6. Номера телефонов\n");
     printf("7. Адреса электронной почты\n");
@@ -149,6 +132,7 @@ void editContact() {
     int choice;
     scanf("%d", &choice);
 
+    // Обновление данных контакта в зависимости от выбора
     switch (choice) {
         case 1:
             printf("Введите новое имя: ");
@@ -163,7 +147,7 @@ void editContact() {
             scanf("%s", contact->thirdname);
             break;
         case 4:
-            printf("Введите новое место работы: ");
+            printf("Введите новую компанию: ");
             scanf("%s", contact->company);
             break;
         case 5:
@@ -225,61 +209,49 @@ void editContact() {
     }
 }
 
-void deleteContact() {
-    if (contact_count == 0) {
-        printf("Список контактов пуст.\n");
-        return;
+void editContact(Node *node, int index, int *count) {
+    if (node != NULL) {
+        editContact(node->left, index, count);
+        if (*count == index) {  
+            printf("Редактирование контакта:\n");
+            editContactData(&node->contact);
+        }
+        (*count)++;
+        editContact(node->right, index, count);
     }
-
-    int index;
-    printf("Введите номер контакта для удаления (от 0 до %d): ", contact_count - 1);
-    scanf("%d", &index);
-
-    if (index < 0 || index >= contact_count) {
-        printf("Неверный номер контакта.\n");
-        return;
-    }
-
-    // Сдвигаем все контакты после удаляемого на одно место влево
-    for (int i = index; i < contact_count - 1; i++) {
-        contacts[i] = contacts[i + 1];
-    }
-    contact_count--;
-    printf("Контакт удален.\n");
 }
 
-void displayContacts() {
-    if (contact_count == 0) {
-        printf("Список контактов пуст.\n");
-        return;
-    }
+Node* deleteNode(Node* root, int index, int *count) {
+    if (root == NULL) return root;
 
-    printf("Список контактов:\n");
-    for (int i = 0; i < contact_count; i++) {
-        printf("Контакт %d:\n", i);
-        printf("Имя: %s\n", contacts[i].firstname);
-        printf("Фамилия: %s\n", contacts[i].lastname);
-        printf("Отчество: %s\n", contacts[i].thirdname);
-        printf("Место работы: %s\n", contacts[i].company);
-        printf("Должность: %s\n", contacts[i].post);
-        printf("Телефоны: ");
-        for (int j = 0; j < contacts[i].phone_count; j++) {
-            printf("%s ", contacts[i].phone_numbers[j]);
+    // Сначала обходим левое поддерево
+    root->left = deleteNode(root->left, index, count);
+    
+    if (*count == index) {
+        // Удаление узла
+        if (root->left == NULL) {
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            Node *temp = root->left;
+            free(root);
+            return temp;
         }
-        printf("\nEmail: ");
-        for (int j = 0; j < contacts[i].email_count; j++) {
-            printf("%s ", contacts[i].emails[j]);
+
+        // Узел с двумя поддеревьями: получить прямого преемника
+        Node *temp = root->right;
+        while (temp->left != NULL) {
+            temp = temp->left;
         }
-        printf("\nСоцсети: ");
-        for (int j = 0; j < contacts[i].social_count; j++) {
-            printf("%s ", contacts[i].social_links[j]);
-        }
-        printf("\nМессенджеры: ");
-        for (int j = 0; j < contacts[i].messenger_count; j++) {
-            printf("%s ", contacts[i].messenger_links[j]);
-        }
-        printf("\n\n");
+
+        // Заменить корень на прямого преемника
+        root->contact = temp->contact;
+        root->right = deleteNode(root->right, *count + 1, count); 
     }
+    
+    (*count)++;
+    return root; 
 }
 
 void menu() {
@@ -299,18 +271,95 @@ int main() {
         scanf("%d", &choice);
         
         switch (choice) {
-            case 1:
-                addContact();
+            case 1: {
+                struct Contact new_person;
+                printf("Введите имя: ");
+                scanf("%s", new_person.firstname);
+                printf("Введите фамилию: ");
+                scanf("%s", new_person.lastname);
+                printf("Введите отчество: ");
+                scanf("%s", new_person.thirdname);
+                printf("Введите компанию (необязательно): ");
+                scanf("%s", new_person.company);
+                printf("Введите должность (необязательно): ");
+                scanf("%s", new_person.post);
+
+                new_person.phone_count = 0;
+                printf("Введите номера телефонов (разделяйте пробелами, введите 'end' для завершения): ");
+                while (1) {
+                    char phone[MAX_PHONE_NUM];
+                    scanf("%s", phone);
+                    if (strcmp(phone, "end") == 0) {
+                        break;
+                    }
+                    strcpy(new_person.phone_numbers[new_person.phone_count++], phone);
+                }
+
+                new_person.email_count = 0;
+                printf("Введите адреса электронной почты (разделяйте пробелами, введите 'end' для завершения): ");
+                while (1) {
+                    char email[MAX_EMAIL_LEN];
+                    scanf("%s", email);
+                    if (strcmp(email, "end") == 0) {
+                        break;
+                    }
+                    strcpy(new_person.emails[new_person.email_count++], email);
+                }
+
+                new_person.social_count = 0;
+                printf("Введите ссылки на соцсети (разделяйте пробелами, введите 'end' для завершения): ");
+                while (1) {
+                    char link[MAX_SOC];
+                    scanf("%s", link);
+                    if (strcmp(link, "end") == 0) {
+                        break;
+                    }
+                    strcpy(new_person.social_links[new_person.social_count++], link);
+                }
+
+                new_person.messenger_count = 0;
+                printf("Введите ссылки на мессенджеры (разделяйте пробелами, введите 'end' для завершения): ");
+                while (1) {
+                    char link[MAX_SOC];
+                    scanf("%s", link);
+                    if (strcmp(link, "end") == 0) {
+                        break;
+                    }
+                    strcpy(new_person.messenger_links[new_person.messenger_count++], link);
+                }
+
+                root = insert(root, new_person);
+                printf("Контакт добавлен.\n");
                 break;
-            case 2:
-                editContact();
+            }
+            case 2: {
+                int count = 0;
+                printf("Введите номер контакта для редактирования:\n");
+                inOrder(root, &count); 
+                printf("Номер контакта: ");
+                int index;
+                scanf("%d", &index);
+                count = 0; 
+                editContact(root, index, &count); 
                 break;
-            case 3:
-                deleteContact();
+            }
+            case 3: {
+                int count = 0;
+                printf("Введите номер контакта для удаления:\n");
+                inOrder(root, &count); 
+                printf("Номер контакта: ");
+                int index;
+                scanf("%d", &index);
+                count = 0; 
+                root = deleteNode(root, index, &count); 
+                printf("Контакт удален.\n");
                 break;
-            case 4:
-                displayContacts();
+            }
+            case 4: {
+                int count = 0;
+                inOrder(root, &count);
                 break;
+            }
             case 5:
                 printf("Выход из программы.\n");
                 return 0;
